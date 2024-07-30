6f23dfd189ff8312609b66c389484c1fa614f726
Hisa <hisashi.konno@terragene.com>
2024-07-30 16:02:36 -0300

    posible thread manager.py, views y urls

--- Module_Manager/views.py
+++ Module_Manager/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .thread_manager import ThreadManager
 
-# Create your views here.
+class QueryView(APIView):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        query = request.data.get('query')
+        
+        if not query:
+            return Response({'error': 'Query is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        thread_manager = ThreadManager()
+        thread = thread_manager.get_or_create_active_thread(user)
+        
+        # Aquí iría la lógica para manejar la query con OpenAI y obtener la respuesta
+        response = self.handle_query(thread, query)
+        
+        return Response({'response': response}, status=status.HTTP_200_OK)
