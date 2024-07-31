af1adcb8de41c534b559b03f0cb8a1110007c155
Hisa <hisashi.konno@terragene.com>
2024-07-31 09:24:21 -0300

    posible thread manager en models.py

--- Module_Manager/models.py
+++ Module_Manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+from django.utils import timezone
 
-# Create your models here.
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_activity = models.DateTimeField(auto_now=True)
+    thread_id = models.CharField(max_length=255, default=uuid.uuid4)  # Agregar un valor predeterminado
+
+    def update_last_activity(self):
+        self.last_activity = timezone.now()
+        self.save()
