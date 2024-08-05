3dff1614a74a2cbe00f02293d60c19b38f781f54
Mateo <mateo.rovere@terragene.com>
2024-08-05 15:41:32 -0300

    funcion aplicada a rag

--- RAG_Manager/services.py
+++ RAG_Manager/services.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -11,14 +12,35 @@ class TechnicalQueryAssistant:
 
     def handle_technical_query(self, query):
         # Make the call to the chat completions endpoint with the assistant ID and ensure it uses the vector store
-        response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
+        chat = self.client.beta.threads.create(
             messages=[
-                {"role": "system", "content": "You are a technical assistant with access to a vector store for retrieving relevant information."},
-                {"role": "user", "content": query}
-            ],
-            tools=[
-                {"type": "file_search"}  # Ensure the retrieval tool is specified
+                {
+                    "role":"user",
+                    "content":f"{query}"
+                }
             ]
         )
-        return response.choices[0].message.content
+
+
+
+        run = self.client.beta.threads.runs.create(
+            thread_id=chat.id,
+            assistant_id=self.assistant_id,
+            tool_choice="auto")
+        print(f"Run Created: {run.id}")
+
+        while run.status != "completed":
+            run = self.client.beta.threads.runs.retrieve(thread_id=chat.id,run_id=run.id)
+            print(f"Run Status: {run.status}")
+            if run.status == "failed":
+                break
+            time.sleep(0.5)
+        
+        if run.status != "failed":
+            print("Run Completed!")
+
+            messages_response = self.client.beta.threads.messages.list(thread_id=chat.id)
+            messages = messages_response.data
+
+            latest_message = messages[0]
+            return f"Response: {latest_message.content[0].text.value}"
