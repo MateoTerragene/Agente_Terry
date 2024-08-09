200fd36942f1ef24069e32e86a4cd343e6b9e193
Mateo <mateo.rovere@terragene.com>
2024-08-09 08:22:44 -0300

    iniciando purchase_oportunity

--- PO_Manager/models.py
+++ PO_Manager/models.py
@@ -1,3 +1,11 @@
+.# po_manager/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.FloatField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
