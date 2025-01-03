8d561eab5298b163c55230f15a816897274f2d9c
Mateo <mateo.rovere@terragene.com>
2025-01-03 15:11:51 -0300

    v1.1.1

--- Agente_Terry/settings.py
+++ Agente_Terry/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['terragene.online', 'www.terragene.online','100.27.3.89', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- showtime.py
+++ showtime.py
@@ -1,1 +1,1 @@
-print("hola_test_cicd")#borrar v. git branch 1.0.27
+print("hola_test_cicd")#borrar v. git branch 1.1.1
