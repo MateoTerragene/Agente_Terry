b26d55cf07e6144b05784ed163159ac6237c4d12
Hisa <hisashi.konno@terragene.com>
2025-01-09 12:52:43 -0300

    set_avatar falta terminar

--- Module_Manager/urls.py
+++ Module_Manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path('whatsapp_query/', WhatsAppQueryView.as_view(), name='whatsapp_query'),
     path('web-service/', ClassifyQueryView.as_view(), name='web_service'),
     path('create-thread/', UserView.as_view(), name='create_thread'),  # Ruta para crear un nuevo thread
+    path('set_avatar/', UserView.as_view(), name='set_avatar'),  # Ruta para configurar el avatar
 ]
